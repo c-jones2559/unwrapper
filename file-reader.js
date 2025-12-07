5e2fead38611ae23ee6917c52fb2dd19e3e33bf0
= <christopher.jones2559@gmail.com>
2025-12-07 23:07:21 +0000

    actually calling the new function

--- file-reader.js
+++ file-reader.js
@@ -126,6 +126,7 @@ function checkValid (songs) {
     console.log("[============] 100%");
     console.log("File valid!");
     valid.innerHTML = "Valid file, processing. (this won't do anything yet)";
+    calculateStats(songs);
     return;
 }
 
