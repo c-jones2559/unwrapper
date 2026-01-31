028cf9be1dbef15b6b12c37c5cb35479ead960a2
Chris Jones <christopher.jones2559@gmail.com>
2026-01-31 17:27:37 +0000

    artists

--- statsProcessor.js
+++ statsProcessor.js
@@ -8,9 +8,11 @@ export function calculateStats(songs) {
 
     const time = totalTime(songs);
     const sortedSongs = topSongs(songs);
+    const sortedArtists = topArtists(songs);
     stats.innerHTML = `Your total listening time is: ${timeFormatter(time)}!<br>
     That's ~${msToHours(time)} or ~${msToDays(time)}!<br>
-    Your top song is ${sortedSongs[0][0]} with ${sortedSongs[0][1]} listens!`
+    Your top song is ${sortedSongs[0][0]} with ${sortedSongs[0][1]} listens!<br>
+    Your top artist is ${sortedArtists[0][0]} with ${sortedArtists[0][1]} listens!`
     console.log("All stats calculated.");
 }
 
@@ -26,7 +28,6 @@ function totalTime(songs) {
 
 function topSongs(songs) {
     console.log("Calculating top songs...");
-
     const topSongs = {};
 
     for (const song of songs) {
@@ -36,7 +37,22 @@ function topSongs(songs) {
     const sortedSongs = Object.entries(topSongs)
         .sort(([, countA], [, countB]) => countB - countA);
 
-
     console.log("Top songs calculated.");
     return sortedSongs;
+}
+
+
+function topArtists(songs) {
+    console.log("Calculating top artists...");
+    const topArtists = {};
+
+    for (const song of songs) {
+        topArtists[song.master_metadata_album_artist_name] = (topArtists[song.master_metadata_album_artist_name] || 0) + 1; 
+    }
+
+    const sortedArtists = Object.entries(topArtists)
+        .sort(([, countA], [, countB]) => countB - countA);
+
+    console.log("Top artists calculated.");
+    return sortedArtists;
 }
