2feda33cab9b3bc1b11a09b124cbad771e746fb2
Chris Jones <christopher.jones2559@gmail.com>
2026-01-31 17:06:07 +0000

    display the top song

--- statsProcessor.js
+++ statsProcessor.js
@@ -7,19 +7,36 @@ function calculateStats(songs) {
 
 
     const time = totalTime(songs);
+    const sortedSongs = topSongs(songs);
     stats.innerHTML = `Your total listening time is: ${timeFormatter(time)}!<br>
-    That's ~${msToHours(time)} or ~${msToDays(time)}!`;
-
-    console.log("All stats calculated.")
+    That's ~${msToHours(time)} or ~${msToDays(time)}!<br>
+    Your top song is ${sortedSongs[0][0]} with ${sortedSongs[0][1]} listens!`
+    console.log("All stats calculated.");
 }
 
-
 function totalTime(songs) {
-    console.log("Calculating time...")
-    let total = 0
+    console.log("Calculating time...");
+    let total = 0;
     for (const song of songs) {
-        total += song.ms_played
+        total += song.ms_played;
     }
-    console.log("Time calculated.")
+    console.log("Time calculated.");
     return total;
+}
+
+function topSongs(songs) {
+    console.log("Calculating top songs...");
+
+    const topSongs = {};
+
+    for (const song of songs) {
+        topSongs[song.master_metadata_track_name] = (topSongs[song.master_metadata_track_name] || 0) + 1; 
+    }
+
+    const sortedSongs = Object.entries(topSongs)
+        .sort(([, countA], [, countB]) => countB - countA);
+
+
+    console.log("Top songs calculated.");
+    return sortedSongs;
 }
